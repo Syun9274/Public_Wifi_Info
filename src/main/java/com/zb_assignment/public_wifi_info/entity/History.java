1a0cffce86563515b740845e72dc0d00322fb48c
YSMin <zxczxv7624@gmail.com>
2024-08-12 23:02:48 +0900

    파일 복구

---
+++ src/main/java/com/zb_assignment/public_wifi_info/entity/History.java
@@ -0,0 +1,54 @@
+package com.zb_assignment.public_wifi_info.entity;
+
+import com.sun.istack.NotNull;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "history")
+public class History {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NotNull
+    private double lat;
+
+    @NotNull
+    private double lnt;
+
+    @NotNull
+    private LocalDateTime date;
+
+
+    // Getter and Setter
+    public int getId() {
+        return id;
+    }
+
+    public double getLat() {
+        return lat;
+    }
+
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
+
+    public double getLnt() {
+        return lnt;
+    }
+
+    public void setLnt(double lnt) {
+        this.lnt = lnt;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+}
